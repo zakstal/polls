f6f7068148e2872d8fc4f8c52d07f39e22e3d376
Zak Stallings <Zakstal@gmail.com>
2014-09-25 15:15:49 -0400

    n + 1 query and better response count query

--- app/models/question.rb
+++ app/models/question.rb
@@ -14,4 +14,58 @@ class Question < ActiveRecord::Base
   )
 
   has_many :responses, through: :answer_choices, source: :responses
+
+  def shitty_results
+    # n + 1
+    results = Hash.new(0)
+    choices = self.answer_choices
+    choices.each do |answer_choice|
+      results[answer_choice] += answer_choice.responses.count
+    end
+    results
+  end
+
+  def less_shitty
+    choice_and_responses = self.answer_choices.includes(:responses)
+    results = Hash.new(0)
+
+    choice_and_responses.each do |answer_choice|
+      results[answer_choice] += answer_choice.responses.length
+    end
+
+    results
+  end
+
+  def the_best
+    # seequel = <<-SQL
+   #    SELECT
+   #      answer_choices.*, COUNT(responses) AS response_count
+   #    FROM
+   #      answer_choices
+   #    LEFT OUTER JOIN
+   #      responses
+   #    ON
+   #      responses.answer_choice_id = answer_choices.id
+   #    WHERE
+   #      answer_choices.question_id = ?
+   #    GROUP BY
+   #      answer_choices.id
+   #  SQL
+   #
+   #  results = AnswerChoice.find_by_sql([seequel, self.id])
+   #
+   #  results.map do |response|
+   #        [response.answer, response.response_count ]
+   #      end
+
+    answer_choices_with_count = self.answer_choices
+        .select("answer_choices.*, COUNT(responses.id) AS response_count")
+        .joins("LEFT OUTER JOIN responses ON responses.answer_choice_id = answer_choices.id")
+        .group("answer_choices.id")
+
+        answer_choices_with_count.map do |response|
+          [response.answer, response.response_count ]
+        end
+  end
+
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -2,6 +2,7 @@ class Response < ActiveRecord::Base
   validates :answer_choice_id, presence: true
   validates :user_id, presence: true
   validate :respondent_has_not_already_answered_question
+  validate :author_cannot_respond_to_own_poll
 
   belongs_to( :responder,
   class_name: 'User',
@@ -26,8 +27,14 @@ class Response < ActiveRecord::Base
   private
 
   def respondent_has_not_already_answered_question
-    if sibling_responses.exists?(:id => self.id)
+    if self.sibling_responses.exists?(:user_id => responder.id)
       errors[:sibling_responses] << "can't have multiple responses"
+    end
+  end
+
+  def author_cannot_respond_to_own_poll
+    if self.question.poll.author.id == self.user_id
+      errors[:author] << "cannot respond to own poll"
     end
   end
 end
